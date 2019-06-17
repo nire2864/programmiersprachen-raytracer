f3d2e28e3e900ba772ee049b52aec98ac00101df
Luigi Portwich <luigi.vinzenz.portwich@uni-weimar.de>
2019-06-17 18:15:44 +0200

    Add sphere volume and test

--- source/tests.cpp
+++ source/tests.cpp
@@ -11,6 +11,13 @@ TEST_CASE("Sphere area", "[area]")
   REQUIRE(s2.area() == Approx(1333.166258f));
 }
 
+TEST_CASE("Sphere volume", "volume")
+{
+  Sphere s3{10.3f,{4.0f,5.5f,2.0f}};
+  
+  REQUIRE(s3.volume() == Approx(4577.204154));
+}
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
